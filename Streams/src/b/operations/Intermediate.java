2b745de7e9a2847ba381cf4b4e9a8032408dda99
Sarath KP <87646653+Sarath343@users.noreply.github.com>
2025-01-29 22:57:21 +0530

    Add files via upload

---
+++ Streams/src/b/operations/Intermediate.java
@@ -0,0 +1,17 @@
+package b.operations;
+
+public class Intermediate {
+	//filter
+	//map
+	//distinct
+	//sorted
+	//limit()
+	//skip()
+	
+	
+	public static void main(String[] args) {
+//		List<String>
+//		doFilter();
+	}
+
+}
