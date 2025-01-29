0bb9924b79fd8eda9a00a1f77fc6c27a5ae30a3e
sarath <sarathkp1998@gmail.com>
2025-01-30 00:06:51 +0530

    added more code

--- Streams/src/b/operations/Intermediate.java
+++ Streams/src/b/operations/Intermediate.java
@@ -1,17 +1,33 @@
 package b.operations;
 
+import java.util.List;
+
 public class Intermediate {
-	//filter
-	//map
-	//distinct
-	//sorted
-	//limit()
-	//skip()
-	
-	
+	// filter
+	// map
+	// distinct
+	// sorted
+	// limit(n) :	Will return first n elements only in the Stream
+	// skip(n)	:	Will discard the first n elements of the Stream
+
 	public static void main(String[] args) {
-//		List<String>
-//		doFilter();
+		List<Integer> lst = List.of(1, 2, 3, 4, 5, 6,7,8, 7, 8,9,2);
+		//doFilter(lst);
+		//doMap(lst);
+		doDistinctMapFilterSort(lst);
+
+	}
+
+	private static void doDistinctMapFilterSort(List<Integer> lst) {
+		lst.stream().distinct().filter(x->x<5).map(x->x*x).sorted().skip(1).limit(2).forEach(System.out::println);
+	}
+
+	private static void doMap(List<Integer> lst) {
+		lst.stream().map(x -> 2 * x).forEach(System.out::println);
+	}
+
+	private static void doFilter(List<Integer> lst) {
+		lst.stream().filter(x -> x < 6).forEach(System.out::println);
 	}
 
 }
---
+++ Streams/src/b/operations/Terminal.java
@@ -0,0 +1,48 @@
+package b.operations;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Terminal {
+	// collect()
+	// count()
+	// findFirst()
+	// findAny()
+	// anyMatch(condition)
+	// allMatch(condition)
+	// noneMatch(condition)
+
+	public static void main(String[] args) {
+		List<Integer> lst = List.of(1, 2, 3, 4, 4, 5, 6, 7, 8, 7, 8, 9, 2);
+
+		// collectAndCount(lst);
+		// doFind(lst);
+		findingMatches(lst);
+
+	}
+
+	private static void findingMatches(List<Integer> lst) {
+		System.out.println(lst.stream().anyMatch(x -> x > 8));
+		System.out.println(lst.stream().allMatch(x->x%2==0));
+		System.out.println(lst.stream().noneMatch(x->x>20));
+
+	}
+
+	private static void doFind(List<Integer> lst) {
+		System.out.println(lst.stream().filter(x -> x > 4).findFirst().get());
+
+		System.out.println(lst.stream().filter(x -> x < 4).findAny().get());
+
+	}
+
+	private static void collectAndCount(List<Integer> lst) {
+		System.out.println(lst.stream().count());
+		List<Integer> collected = lst.stream().distinct().collect(Collectors.toList());
+		// collected.forEach(System.out::println);
+		Set<Integer> sortedCollected = lst.stream().sorted().collect(Collectors.toSet());
+		sortedCollected.forEach(System.out::println);
+
+	}
+
+}
