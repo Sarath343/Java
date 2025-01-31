a94048ba779138aea21cf2749d8609b239202035
Sarath KP <87646653+Sarath343@users.noreply.github.com>
2025-01-31 18:34:46 +0530

    Update MostFrequentWord.java

--- Streams/src/e/exercises/MostFrequentWord.java
+++ Streams/src/e/exercises/MostFrequentWord.java
@@ -1,5 +1,6 @@
 package e.exercises;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,13 +26,13 @@ public class MostFrequentWord {
 		List<String> lst = List.of(sentence.split(" "));
 		Map<Object, Long> grouped = lst.stream().collect(Collectors.groupingBy(n->n , Collectors.counting()));
 		System.out.println(grouped);
+
+		//advanced 
+		String mostOccName = 
+		lst.stream().collect(Collectors.groupingBy(n->n , Collectors.counting()))
+		.entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).orElse(null);
+		System.out.println(mostOccName);
 	}
-
-
-
-
-
-
 
 
 
